743dc35401e648e14b3b3f620b6f38d7ef3aabfd
Felipe Echeverri <fecheverri>
2019-10-29 16:10:27 -0500

    CON LAZYLOAD FUNCIONAL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', loadChildren: './pages/index/index.module#IndexModule' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/index/index.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IndexComponent } from './index.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [  
+  { path: '', component: IndexComponent },
+];
+@NgModule({
+  declarations: [IndexComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class IndexModule { }
