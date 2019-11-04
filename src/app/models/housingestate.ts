7d4652d4b9ff1516cf88072260a53dcd94ad0b2e
Felipe Echeverri <fecheverri>
2019-11-04 13:00:11 -0500

    CORRECCIÓN DE ERRORES Y PRIMER DESPLIEGE

--- src/app/models/housingestate.ts
+++ src/app/models/housingestate.ts
@@ -4,6 +4,8 @@ export class HousingEstate {
     description: string;
     location: string;
     address: string;
+    phone_number: string;
+    police_phone_number: string;
     neighborhood: string;
     apartments: [];
     gates: [];
