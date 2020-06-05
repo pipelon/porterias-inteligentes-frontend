a5a62439e23e75b37c4f40e8fa2155284824c93c
Pipe Echeverri <pipe.echeverri.1@gmail.com>
2020-06-05 08:22:58 -0500

    SOCKET LISTO DEPENDIENDO DEL USUARIO

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
 import { environment } from 'src/environments/environment';
 import { Observable, Observer } from 'rxjs';
+import { StorageService } from './../auth/storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +11,22 @@ export class SocketService {
 
   public socket;
   observer: Observer<any>;
+  public session;
 
-  constructor() { }
+  constructor(private _storageService: StorageService) {
+    this.session = this._storageService.getCurrentSession();
+  }
 
   setupSocketConnection(): Observable<any> {
     this.socket = io(environment.SOCKET_ENDPOINT);
 
-    this.socket.on('AlertaHorus', (data) => {
-      this.observer.next(data);
+    this.socket.on('alertas_generales', (data) => {
+      if (this.session.id === data.userId) {
+        this.observer.next(data);
+      }
     });
-    return this.createObservable();    
+    
+    return this.createObservable();
   }
 
   createObservable(): Observable<number> {
